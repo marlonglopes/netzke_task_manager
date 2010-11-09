54cce2ae24055f07cefe29a4edf1b65af4485cb8
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-11-09 17:10:46 -0300

    teste

---
+++ app/models/task.rb
@@ -0,0 +1,6 @@
+class Task < ActiveRecord::Base
+
+	validates_presence_of :name
+	default_scope :conditions => {:done => false}
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetzkeTaskManager::Application.config.secret_token = '3d728922a3512593d479760eeacac304b387c593032cfd50c832752ee02b2968ba53dcb506a2ae93afa0891caaebfcace329385603b8c9d4af6500bb27bfe997'
